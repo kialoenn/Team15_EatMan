c4291f03ddb4f0e907a1c6a6eab4ab86f7373a3a
Man Sun <msun45@my.bcit.ca>
2021-03-24 10:00:49 -0700

    edit coding style

---
+++ scripts/function.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAsS-NgdLNtD_wy0UGftvZ5O4dDeWn4cC8",
+    authDomain: "man-s-eat.firebaseapp.com",
+    projectId: "man-s-eat",
+    storageBucket: "man-s-eat.appspot.com",
+    messagingSenderId: "784972971331",
+    appId: "1:784972971331:web:29d991281ede35e2e0c3f6",
+    measurementId: "G-EE7LR9RRVM"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
