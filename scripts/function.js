d3cb9c9d8fb3952c085719f0d27a885b63948903
Man Sun <msun45@my.bcit.ca>
2021-03-26 14:39:33 -0700

    change name convension

--- scripts/function.js
+++ scripts/function.js
@@ -16,7 +16,7 @@ function sayHello() {
 sayHello();
 
 function writeRestaurants() {
-    var restaurantsRef = db.collection("restaurants");
+    var restaurantsRef = db.collection("RESTAURANT");
     restaurantsRef.add({
         RESTAURANT_ID: "CAT1",
         RESTAURANT_NAME: "Catcus Club Cafe",
@@ -25,5 +25,29 @@ function writeRestaurants() {
         RESTAURANT_PICTURE: "CAT1.jpg",
         RESTAURANT_TIME: "8:00AM TO 10:30PM",
     });
+    restaurantsRef.add({
+        RESTAURANT_ID: "CAT2",
+        RESTAURANT_NAME: "Catcus Club Cafe",
+        RESTAURANT_ID: "6090 Silver Dr",
+        RESTAURANT_PHONE: "(604)291-9339",
+        RESTAURANT_PICTURE: "CAT2.jpg",
+        RESTAURANT_TIME: "8:00AM TO 10:30PM",
+    });
+    restaurantsRef.add({
+        RESTAURANT_ID: "CAT3",
+        RESTAURANT_NAME: "Catcus Club Cafe",
+        RESTAURANT_ID: "4219 B Lougheed Hwy",
+        RESTAURANT_PHONE: "(604)291-6606",
+        RESTAURANT_PICTURE: "CAT3.jpg",
+        RESTAURANT_TIME: "8:00AM TO 10:30PM",
+    });
+    restaurantsRef.add({
+        RESTAURANT_ID: "CAFE106",
+        RESTAURANT_NAME: "Cafe 106",
+        RESTAURANT_ID: "6588 Royal Oak Ave Unit 106",
+        RESTAURANT_PHONE: "(604)438-1220",
+        RESTAURANT_PICTURE: "CAFE106.jpg",
+        RESTAURANT_TIME: "11:00AM TO 11:30PM",
+    });
 }
-writeRestaurants();
+//writeRestaurants();
