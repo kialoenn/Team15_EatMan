3528f0d3b538bc18b3e1ae9fe4710e612ece2683
Man Sun <msun45@my.bcit.ca>
2021-03-26 14:22:46 -0700

    add database

--- scripts/function.js
+++ scripts/function.js
@@ -1,11 +1,29 @@
-var firebaseConfig = {
-    apiKey: "AIzaSyAsS-NgdLNtD_wy0UGftvZ5O4dDeWn4cC8",
-    authDomain: "man-s-eat.firebaseapp.com",
-    projectId: "man-s-eat",
-    storageBucket: "man-s-eat.appspot.com",
-    messagingSenderId: "784972971331",
-    appId: "1:784972971331:web:29d991281ede35e2e0c3f6",
-    measurementId: "G-EE7LR9RRVM"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+function sayHello() {
+    firebase.auth().onAuthStateChanged(function (hello) {
+        if (hello) {
+            console.log(hello.uid);
+            db.collection("ACCOUNT")
+                .doc(hello.uid)
+                .get()
+                .then(function (doc) {
+                    console.log(doc.data().name);
+                    var name = doc.data().name;
+                    $("#username").text(name);
+                })
+        }
+    })
+}
+sayHello();
+
+function writeRestaurants() {
+    var restaurantsRef = db.collection("restaurants");
+    restaurantsRef.add({
+        RESTAURANT_ID: "CAT1",
+        RESTAURANT_NAME: "Catcus Club Cafe",
+        RESTAURANT_ID: "7320 Market Crossing",
+        RESTAURANT_PHONE: "(604)430-5000",
+        RESTAURANT_PICTURE: "CAT1.jpg",
+        RESTAURANT_TIME: "8:00AM TO 10:30PM",
+    });
+}
+writeRestaurants();
