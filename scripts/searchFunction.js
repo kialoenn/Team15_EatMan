b1778eee620d5488df119ef0b72055c8b67178d8
Man Sun <msun45@my.bcit.ca>
2021-04-12 20:54:12 -0700

    update search function

---
+++ scripts/searchFunction.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    console.log("read");
+
+    function getSearch() {
+        document.getElementById("searchButton").addEventListener('click', function () {
+            var pattern = document.getElementById("pattern").value;
+
+            queryResultByName(pattern);
+            //queryResultByCuisine(pattern);
+        })
+    }
+    getSearch();
+
+    function queryResultByName(pattern) {
+        db.collection("restaurants")
+            .where("name", "==", pattern)
+            .get()
+            .then(function (snap) {
+                snap.forEach(function (doc) {
+                    
+                })
+            })
+
+    }
+})
